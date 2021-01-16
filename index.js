d9bdbb85fac0953a357d6e581a089c58146d6534
shenny0916 <75678442+shenny0916@users.noreply.github.com>
2021-01-16 19:11:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+async function changeMenuHeight(){
+    document.getElementsByClassName('menu')[0].style.height = `${$(document).height()}px`
+
+    console.log(document.getElementsByClassName('menu')[0].style.height)
+}
+
 function changePage(num){
     // 1 is home
     // 2 is commands
@@ -5,21 +12,61 @@ function changePage(num){
     if(num === 1){
         //home
 
+        let cmds = document.getElementsByClassName('cmd')
+        let cmdsCat = document.getElementsByClassName('cmd_cat')
+
+        for(var i = 0;i < cmds.length;i++){
+            cmds[i].style.fontSize = '0px'
+        }
+
+        for(var i = 0;i < cmdsCat.length;i++){
+            cmdsCat[i].style.fontSize = '0px'
+        }
+
         document.getElementById('home').style.color = 'rgb(219, 69, 69)'
         document.getElementById('commands').style.color = 'white'
 
         document.getElementsByClassName('home_page')[0].style.visibility = 'visible'
         document.getElementsByClassName('commands_page')[0].style.visibility = 'hidden'
+
+        changeMenuHeight()
     } else
 
     if(num === 2){
         //commands
+        let cmds = document.getElementsByClassName('cmd')
+        let cmdsCat = document.getElementsByClassName('cmd_cat')
+
+        for(var i = 0;i < cmds.length;i++){
+            if(
+            $(document).width() < 801 &&
+            $(document).width() > 544
+            ){
+            cmds[i].style.fontSize = '35px'
+            } else
+
+            if(
+            $(document).width() < 801 &&
+            $(document).width() < 546
+            ){
+            cmds[i].style.fontSize = '30px'
+            } else
+            {
+            cmds[i].style.fontSize = '35px'
+            }
+        }
+
+        for(var i = 0;i < cmdsCat.length;i++){
+            cmdsCat[i].style.fontSize = '50px'
+        }
 
         document.getElementById('home').style.color = 'white'
         document.getElementById('commands').style.color = 'rgb(219, 69, 69)'
 
         document.getElementsByClassName('home_page')[0].style.visibility = 'hidden'
         document.getElementsByClassName('commands_page')[0].style.visibility = 'visible'
+
+        changeMenuHeight()
     }
 }
 
@@ -29,7 +76,11 @@ function menu(op){
 
     if(op === 'open'){
         if(running === true) return;
+
+        document.getElementsByClassName('menu')[0].style.height = `${$(document).height()}px`
+
         running = true
+
         menu.style.visibility = 'visible'
         menu.classList.add('move-right-and-show-menu')
 
@@ -46,6 +97,7 @@ function menu(op){
         }, 1999);
     } else {
         if(running === true) return;
+
         document.getElementsByClassName('options')[0].classList.add('invis')
 
         setTimeout(() => {
@@ -59,6 +111,9 @@ function menu(op){
             menu.style.visibility = 'hidden'
             running = false
             menu.classList.remove('move-left-and-hide-menu')
+
+
+            document.getElementsByClassName('menu')[0].style.height = `0px`
         }, 2000);
     }
 }
